64cb79a36e97599075ecb9084faefbc30ac1f621
shariar-duke <shariarmahmud225@gmail.com>
2024-06-21 02:50:35 +0600

    app.jsx with header and weatherBoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
 
 export default function App() {
   return (
     <>
-      <Header />
+      <div className="grid place-items-center h-screen">
+        <Header />
+        <main>
+          <section>
+            <WeatherBoard />
+          </section>
+        </main>
+      </div>
     </>
   );
 }
