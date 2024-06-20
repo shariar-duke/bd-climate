9e1d754d9ec6f63fda429bd1d2f19f565525a22b
shariar-duke <shariarmahmud225@gmail.com>
2024-06-21 01:10:37 +0600

    logo component aadded

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,8 @@
+import LogoImage from "../../assets/logo.svg"
+export default function Logo() {
+  return (
+    <a href="#">
+        <img src={LogoImage} alt="weather" className="h-9" />
+    </a>
+  )
+}
