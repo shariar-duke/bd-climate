d06cef8282e325b2ca2ae86d6c0a4ce1ecddea7c
shariar-duke <shariarmahmud225@gmail.com>
2024-06-21 15:00:12 +0600

    Weather condition dynamic data these are the data of right side

--- src/components/weather/WeatherCondition.jsx
+++ src/components/weather/WeatherCondition.jsx
@@ -3,25 +3,35 @@ import HumidityIcon from "../../assets/icons/humidity.svg";
 import TempMaxIcon from "../../assets/icons/temp-max.svg";
 import TempMinIcon from "../../assets/icons/temp-min.svg";
 import WindIcon from "../../assets/icons/wind.svg";
-
+import { useContext } from "react";
+import { WeatherContext } from "../../context";
 export default function WeatherCondition() {
+  const { weatherData } = useContext(WeatherContext);
+  const {
+    maxTemperature,
+    minTemperature,
+    humidity,
+    cloudPercentage,
+    wind,
+    climate,
+} = weatherData;
   return (
     <div>
       <p className="text-sm lg:text-lg font-bold uppercase mb-8">
-        thunderstrom with light drizzle
+      The climate is <u>{climate}</u>
       </p>
       <ul className="space-y-6">
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
             <span>Temp max</span>
             <div className="inline-flex space-x-4">
-                <p> 19° </p>
+                <p> {maxTemperature}° </p>
                 <img src={TempMaxIcon} alt="temp-max" />
             </div>
         </li>
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
             <span>Temp min </span>
             <div className="inline-flex space-x-4">
-                <p> 19° </p>
+                <p> {minTemperature}° </p>
                 <img src={TempMinIcon} alt="temp-max" />
             </div>
         </li>
@@ -29,7 +39,7 @@ export default function WeatherCondition() {
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
             <span> Humadity </span>
             <div className="inline-flex space-x-4">
-                <p> 58% </p>
+                <p> {humidity}% </p>
                 <img src={HumidityIcon} alt="temp-max" />
             </div>
         </li>
@@ -37,7 +47,7 @@ export default function WeatherCondition() {
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
             <span> Cloudy </span>
             <div className="inline-flex space-x-4">
-                <p> 86% </p>
+                <p> {cloudPercentage}% </p>
                 <img src={CloudyIcon} alt="temp-max" />
             </div>
         </li>
@@ -45,7 +55,7 @@ export default function WeatherCondition() {
         <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
             <span> Wind</span>
             <div className="inline-flex space-x-4">
-                <p> 5km/h </p>
+                <p> {wind}km/h </p>
                 <img src={WindIcon} alt="temp-max" />
             </div>
         </li>
