130ad4f6c5d28d3a8802d5ae2470039806f72f0e
shariar-duke <shariarmahmud225@gmail.com>
2024-06-21 16:43:32 +0600

    toggle button activated in WeatherBoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
-import { WeatherProvider } from "./provider";
+import { WeatherProvider, FavouriteProvider } from "./provider";
 export default function App() {
   return (
     <WeatherProvider>
-      <div className="grid place-items-center h-screen">
-        <Header />
-        <main>
-          <section>
-            <WeatherBoard />
-          </section>
-        </main>
-      </div>
+      <FavouriteProvider>
+        <div className="grid place-items-center h-screen">
+          <Header />
+          <main>
+            <section>
+              <WeatherBoard />
+            </section>
+          </main>
+        </div>
+      </FavouriteProvider>
     </WeatherProvider>
   );
 }
--- src/components/weather/AddToFavourite.jsx
+++ src/components/weather/AddToFavourite.jsx
@@ -1,12 +1,22 @@
 import HeartIcon from "../../assets/heart.svg"
-
+import RedHearIcon from "../../assets/heart-red.svg"
+import { useContext, useState } from "react"
+import { FavouriteContext } from "../../context"
 export default function AddToFavourite() {
+  const { favourities , addToFavourites, removeFromFavourites} =  useContext(FavouriteContext)
+  const[isFavourite ,setIsFavourite ] = useState(false)
+
+  const handleFavourte =()=> 
+  {
+    setIsFavourite(!isFavourite)
+  }
   return (
     <div className="md:col-span-2">
         <div className="flex items-center justify-end space-x-6">
-            <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
+            <button onClick={handleFavourte}
+            className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
                 <span>Add to Favourite</span>
-                <img src={HeartIcon} alt="" />
+                <img src={isFavourite ? RedHearIcon: HeartIcon} alt="" />
             </button>
         </div>
     </div>
