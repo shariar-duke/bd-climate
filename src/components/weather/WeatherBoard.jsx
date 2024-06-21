45651ebd4bed24a7ad35316dec3b45e73e994cec
shariar-duke <shariarmahmud225@gmail.com>
2024-06-21 10:06:40 +0600

    got data in WeatherBoar compoenent from hook

--- src/components/weather/WeatherBoard.jsx
+++ src/components/weather/WeatherBoard.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import AddToFavourite from './AddToFavourite'
 import WeatherHeadLine from './WeatherHeadLine'
 import WeatherCondition from './WeatherCondition'
-
+import {useWeather} from '../../hooks' // hook ta import korlam 
 export default function WeatherBoard() {
+
+  const {loading, error, weatherData} = useWeather() // hook theke data gula import korlam 
+  console.log(error)
+  console.log(loading)
+  console.log(weatherData)
   return (
     <div className='container'>
         <div className='grid bg-black/20 rounded-xl backdrop:blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto'>
