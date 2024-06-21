58b90c5f048488190d7abed62c5f4954bec6ee5e
shariar-duke <shariarmahmud225@gmail.com>
2024-06-21 14:45:34 +0600

    correction in date format

--- src/utils/date-util.js
+++ src/utils/date-util.js
@@ -1,34 +1,29 @@
 function getFormattedDate(value, type, inMS) {
-  // jode type na dia thke tahle ovbe return hbe
-  if (!type) return value;
-  // jode milisecond a na dia thake thle ms a convert korbo
-  if (!inMS) {
-    value = value * 1000;
-  }
+    if(!type) return value;
 
-  const date = new Date(value); // eta miliseconds take nai Date a convert korlo
+    if (!inMS) {
+        value = value * 1000;
+    }
 
-  let options;
-  // ei option tay ei 4 ta option a date dekhabe
-  if (type == "date") {
-    options = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-  } 
-  // ei option a ei dui category te Date a convert hbe 
-  else type == "time";
-  {
-    options = {
-      hour: "numeric",
-      minute: "numeric",
-    };
-  }
+    const date = new Date(value);
+    let options;
 
-  return new Intl.DateTimeFormat("en-us", options).format(date);
+    if (type === 'date') {
+        options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        };
+    } else if(type === 'time') {
+        options = {
+            hour: "numeric",
+            minute: "numeric",
+          };
+    }
+
+
+    return new Intl.DateTimeFormat("en-us", options).format(date);
 }
 
-
-export {getFormattedDate}
+export {getFormattedDate};
