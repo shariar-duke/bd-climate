fdf35af5d4dbf2f5a8f3b948408ecef5e8d96e31
shariar-duke <shariarmahmud225@gmail.com>
2024-06-21 16:16:28 +0600

    FavourtieProvider created

---
+++ src/provider/FavouriteProvider.jsx
@@ -0,0 +1,28 @@
+import { FavouriteContext } from "../context";
+import { useLocalStorage } from "../hooks";
+const FavouriteProvider = ({ children }) => {
+  // ekta lcoal storage create korlam jar key holo favourites r value holo empty array
+  const [favourities, setFavourities] = useLocalStorage("favourites", []);
+
+  const addToFavourites = (latitude, longitude, location) => {
+    setFavourities(...favourities, {
+      latitude: latitude,
+      longitude: longitude,
+      location: location,
+    });
+  };
+
+  const removeFromFavourites = (location) => {
+    const restFavourites = favourities.filter(
+        (fav) => fav.location !== location
+    );
+    setFavourities(restFavourites);
+};
+  return (
+    <FavouriteContext.Provider value={{ favourities , addToFavourites, removeFromFavourites}}>
+      {children}
+    </FavouriteContext.Provider>
+  );
+};
+
+export default FavouriteProvider
