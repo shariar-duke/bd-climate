5625e52ec78f0184101b8baafd7adb2576871885
shariar-duke <shariarmahmud225@gmail.com>
2024-06-22 17:39:14 +0600

    FavouriteList modal select done

--- src/components/header/FavouriteListModal.jsx
+++ src/components/header/FavouriteListModal.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { FavouriteContext } from "../../context";
+import { FavouriteContext , LocationContext } from "../../context";
 import { useContext } from "react";
 
 export default function FavouriteListModal() {
-  const {favourities} = useContext(FavouriteContext)
-  console.log(favourities)
+  const { favourities } = useContext(FavouriteContext);
+  const {setSelectedLocation} = useContext(LocationContext)
+  console.log("The favourites are", favourities)
+
   return (
     <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg">
       <h3 className="text-lg font-bold px-4">Favourite Locations</h3>
       <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-        {
-          favourities.length > 0 ?
-          favourities.map((fav)=> 
-          <li key={fav.location} className="hover:bg-gray-200">{fav.location}</li>
-          ) :
-          <p>
-            Notghing is added to Favourities
-          </p>
-        }
-   
-
+        {favourities.length > 0 ? (
+          favourities.map((fav) => (
+            <li key={fav.location} className="hover:bg-gray-200">
+              {/* onClick a context a favourite er ekta value dia dilam  */}
+              <a onClick={()=>setSelectedLocation({...fav})}>{fav.location}</a>
+            </li>
+          ))
+        ) : (
+          <p>Notghing is added to Favourities</p>
+        )}
       </ul>
     </div>
   );
