1a98213dd2dc159708811483fd7c4a90f6d5e031
shariar-duke <shariarmahmud225@gmail.com>
2024-06-22 17:08:34 +0600

    Location Provider wrap should be most outler line

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
-import { WeatherProvider, FavouriteProvider, LocationProvider } from "./provider";
+import {
+  WeatherProvider,
+  FavouriteProvider,
+  LocationProvider,
+} from "./provider";
 export default function App() {
   return (
-
-    <WeatherProvider>
-      <LocationProvider>
-      <FavouriteProvider>
-        <div className="grid place-items-center h-screen">
-          <Header />
-          <main>
-            <section>
-              <WeatherBoard />
-            </section>
-          </main>
-        </div>
-      </FavouriteProvider>
-      </LocationProvider>
-
-    </WeatherProvider>
-
-
+    <LocationProvider>
+      <WeatherProvider>
+        <FavouriteProvider>
+          <div className="grid place-items-center h-screen">
+            <Header />
+            <main>
+              <section>
+                <WeatherBoard />
+              </section>
+            </main>
+          </div>
+        </FavouriteProvider>
+      </WeatherProvider>
+    </LocationProvider>
   );
 }
