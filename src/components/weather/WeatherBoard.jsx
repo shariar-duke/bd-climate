f20af727a6d023ec9ccb629e630a0f18bb77707d
shariar-duke <shariarmahmud225@gmail.com>
2024-06-22 18:49:36 +0600

    handleing loading in the page.jsx

--- src/components/weather/WeatherBoard.jsx
+++ src/components/weather/WeatherBoard.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import AddToFavourite from "./AddToFavourite";
 import WeatherHeadLine from "./WeatherHeadLine";
 import WeatherCondition from "./WeatherCondition";
-import { useContext } from "react"; // use Conte4xt
-import { WeatherContext } from "../../context"; // weather context ta import korlam
+
 export default function WeatherBoard() {
-  const { weatherData, loading } = useContext(WeatherContext);
+
 
   console.log("The weather data from the conext", weatherData);
 
@@ -13,15 +12,11 @@ export default function WeatherBoard() {
     <div className="container">
       <div className="grid bg-black/20 rounded-xl backdrop:blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
         <div className="grid md:grid-cols-2 md:gap-6">
-          {loading.state ? (
-            <p>Loading ...</p>
-          ) : (
-            <>
-              <AddToFavourite />
-              <WeatherHeadLine />
-              <WeatherCondition />
-            </>
-          )}
+    
+            <AddToFavourite />
+            <WeatherHeadLine />
+            <WeatherCondition />
+         
         </div>
       </div>
     </div>
--- src/page.jsx
+++ src/page.jsx
@@ -1,15 +1,25 @@
-
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
+import { useContext } from "react";
+import { WeatherContext } from "./context";
 export default function Page() {
+  const { loading } = useContext(WeatherContext);
   return (
-    <div className="grid place-items-center h-screen">
-      <Header />
-      <main>
-        <section>
-          <WeatherBoard />
-        </section>
-      </main>
-    </div>
+    <>
+      {loading.state ? (
+        <div className="flex h-full justify-center items-center">
+          <p>{loading.message}</p>
+        </div>
+      ) : (
+        <div className="grid place-items-center h-screen">
+          <Header />
+          <main>
+            <section>
+              <WeatherBoard />
+            </section>
+          </main>
+        </div>
+      )}
+    </>
   );
 }
