f5c872b8399cb92d8741f6e80c16a54815376f64
shariar-duke <shariarmahmud225@gmail.com>
2024-06-22 14:40:09 +0600

    Dynamic data added on FavouriteListModal from localStorage and context

--- src/components/header/FavouriteListModal.jsx
+++ src/components/header/FavouriteListModal.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { FavouriteContext } from "../../context";
+import { useContext } from "react";
 
 export default function FavouriteListModal() {
+  const {favourities} = useContext(FavouriteContext)
+  console.log(favourities)
   return (
     <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg">
       <h3 className="text-lg font-bold px-4">Favourite Locations</h3>
       <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-        <li className="hover:bg-gray-200">Dhaka</li>
-        <li className="hover:bg-gray-200">Rangpur</li>
-        <li className="hover:bg-gray-200">Europe</li>
-        <li className="hover:bg-gray-200">Sylhet</li>
+        {
+          favourities.length > 0 ?
+          favourities.map((fav)=> 
+          <li key={fav.location} className="hover:bg-gray-200">{fav.location}</li>
+          ) :
+          <p>
+            Notghing is added to Favourities
+          </p>
+        }
+   
+
       </ul>
     </div>
   );
